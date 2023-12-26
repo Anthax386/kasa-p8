6d7f294226953b0bffd3c2b85e32dc34afb1b832
Anthax386 <kevinanthax@protonmail.com>
2023-12-26 21:00:18 +0100

    Ajout + configuration du router

--- src/index.jsx
+++ src/index.jsx
@@ -1,16 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import Home from './pages/Home';
-import reportWebVitals from './reportWebVitals';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <Home />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Home from './pages/Home/'
+import Header from './components/Header'
+import Error from './components/Error'
+import FicheLogement from './pages/FicheLogement'
+import Propos from './pages/Propos'
+ 
+ReactDOM.render(
+    <React.StrictMode>
+        <Router>
+            <Header />
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path='/FicheLogement' element={<FicheLogement />}/>
+                <Route path='/propos' element={<Propos />} />
+                <Route path='*' element={<Error />} />
+            </Routes>
+                  </Router>
+    </React.StrictMode>,
+document.getElementById('root')
+)
