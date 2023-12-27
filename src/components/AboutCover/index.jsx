b29d2e6f71067d3b0b2799881b5ed7c3f745169c
Anthax386 <kevinanthax@protonmail.com>
2023-12-27 18:41:26 +0100

    abourCover Component

---
+++ src/components/AboutCover/index.jsx
@@ -0,0 +1,11 @@
+import cover from '../../assets/aboutBanner.png'
+
+function AboutCover () {
+    return (
+        <div className="aboutCover">
+            <img src={cover} alt="cover" />
+        </div>
+    )
+}
+
+export default AboutCover
