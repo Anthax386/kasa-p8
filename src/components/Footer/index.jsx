6284ad1fe3b2e4dfa100a221dede975e4dc333d1
Anthax386 <kevinanthax@protonmail.com>
2023-12-27 18:39:30 +0100

    footer component

---
+++ src/components/Footer/index.jsx
@@ -0,0 +1,14 @@
+import logo from '../../assets/logoWhite.png'
+
+function Footer () {
+    return (
+        <footer>
+            <div>
+                <img src={ logo } alt="logo" />
+                <p>© 2020 Kasa. All rights reserved</p>
+            </div>
+        </footer>
+    )
+}
+
+export default Footer
