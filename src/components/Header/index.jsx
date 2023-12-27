8f93c54be531cfd7c32401d05b139cf2331923ed
Anthax386 <kevinanthax@protonmail.com>
2023-12-27 18:39:17 +0100

    header component

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,11 +1,19 @@
 import { Link } from 'react-router-dom'
+import logo from '../../assets/logo.png'
  
 function Header() {
     return (
-        <nav>
-            <Link to="/">Accueil</Link>
-            <Link to="/propos">A Propos</Link>
-        </nav>
+        <div className='Header-div'>
+            <header>
+                <img src={logo} alt="logo" />
+                <nav>
+                    <Link to="/">Accueil</Link>
+                    <Link to="/propos">A Propos</Link>
+                </nav>
+            </header>
+        </div>
+        
+        
     )
 }
 
