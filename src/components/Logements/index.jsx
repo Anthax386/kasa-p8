e67628b16448f8d554ca8921eefdfe5f78e71560
Anthax386 <kevinanthax@protonmail.com>
2023-12-27 18:38:51 +0100

    component logement

---
+++ src/components/Logements/index.jsx
@@ -0,0 +1,20 @@
+import { Link } from 'react-router-dom'
+import logements from '../../data/logements.json'
+
+function CarteLogement () {
+    return (
+        <section className='logements-section'>
+            {logements.map((logement) => {
+                return (
+                    <article key={logement.id} className='logement-card'>
+                        <Link to={`/logement/${logement.id}`} className='logement-card-link'/>
+                        <img src={logement.cover} alt={logement.title} />
+                        <p>{logement.title}</p>
+                    </article>
+                )
+            })}
+        </section>
+    )
+}
+
+export default CarteLogement
