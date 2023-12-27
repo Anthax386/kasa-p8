af4459618962643283f601f79a5306013d50a8c4
Anthax386 <kevinanthax@protonmail.com>
2023-12-27 18:42:19 +0100

    adding all route

--- src/index.jsx
+++ src/index.jsx
@@ -1,23 +1,26 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import './Styles/main.scss'
 import Home from './pages/Home/'
 import Header from './components/Header'
-import Error from './components/Error'
+import Error from './pages/Error'
 import FicheLogement from './pages/FicheLogement'
 import Propos from './pages/Propos'
- 
+import Footer from './components/Footer'
+
 ReactDOM.render(
     <React.StrictMode>
         <Router>
             <Header />
             <Routes>
                 <Route path="/" element={<Home />} />
-                <Route path='/FicheLogement' element={<FicheLogement />}/>
+                <Route path="/logement/:idLogement" element={<FicheLogement />} />
                 <Route path='/propos' element={<Propos />} />
                 <Route path='*' element={<Error />} />
             </Routes>
-                  </Router>
+            <Footer />
+        </Router>
     </React.StrictMode>,
 document.getElementById('root')
 )
