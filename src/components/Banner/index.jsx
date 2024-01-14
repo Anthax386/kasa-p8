1e3cd0f13855f51a75bbf9229ad6ea98ccd45731
Anthax386 <kevinanthax@protonmail.com>
2024-01-14 11:47:56 +0100

    edit banner component

--- src/components/Banner/index.jsx
+++ src/components/Banner/index.jsx
@@ -2,7 +2,7 @@ function Banner ({img, title}) {
     return (
         <div className='cover'>
             <img src={img} alt='Kasa' id='BannerImg'/>
-            {title ? <h1 id='BannerTitle'>{title}</h1> : null}
+            {title && <h1 id='BannerTitle'>{title}</h1>}
         </div>
     )
 }
