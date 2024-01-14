8b8798130cbf7a923d1055a0b6a48ba95087ce06
Anthax386 <kevinanthax@protonmail.com>
2024-01-14 11:48:19 +0100

    edit collapse component

--- src/components/Collapse/index.jsx
+++ src/components/Collapse/index.jsx
@@ -1,42 +1,39 @@
-function Collapse ({collapses}) {
+import { useState } from "react"
 
-    async function openCloseCollapse(e) {
+function Collapse ({collapse}) {
 
-        const arrow = document.getElementById(e.name)
-        const txt = document.getElementById(e.name+'text')
+    const [isOpen, setOpen] = useState (false)
 
-        if (e.isOpen === false) {
-            e.isOpen = true
+    async function openCloseCollapse() {
+
+        const arrow = document.getElementById(collapse.name)
+        const txt = document.getElementById(collapse.name+'text')
+
+        if (isOpen === false) {
+            setOpen(true)
             arrow.style.animation = 'arrowDown 150ms ease-in-out forwards'
             txt.classList.remove('hidden')
             txt.style.animation = 'showText 150ms ease-in-out forwards'
         } else {
-            e.isOpen = false
+            setOpen(false)
             arrow.style.animation = 'arrowUp 150ms ease-in-out forwards'
             txt.classList.add('hidden')
             txt.style.animation = 'hideText 150ms ease-in-out forwards'
         }
-
     }
 
     return (
-        <section className="Dropdowm-div">
-            {collapses.map((e) => { 
-                return (
-                    <article key={e.name} className="dropdown">
-                        <div className="dropdown-title" onClick={() => openCloseCollapse(e)}>
-                            <p>{e.name}</p>
-                            <svg xmlns="http://www.w3.org/2000/svg" width="33" height="32" viewBox="0 0 33 32" fill="none" id={e.name}>
-                                <path d="M15.2897 10.7897C15.9591 10.1202 17.0462 10.1202 17.7157 10.7897L27.9979 21.0719C28.6674 21.7414 28.6674 22.8285 27.9979 23.4979C27.3285 24.1673 26.2414 24.1673 25.572 23.4979L16.5 14.4259L7.42804 23.4926C6.75862 24.162 5.67148 24.162 5.00206 23.4926C4.33265 22.8231 4.33265 21.736 5.00206 21.0666L15.2843 10.7843L15.2897 10.7897Z" fill="white"/>
-                            </svg>    
-                        </div>
-                        <div className="dropdown-text hidden" id={e.name+'text'}>
-                            <p>{e.txt}</p>
-                        </div>
-                    </article>
-                )
-            })}
-        </section>
+        <article key={collapse.name} className="dropdown">
+            <div className="dropdown-title" onClick={() => openCloseCollapse()}>
+                <p>{collapse.name}</p>
+                <svg xmlns="http://www.w3.org/2000/svg" width="33" height="32" viewBox="0 0 33 32" fill="none" id={collapse.name}>
+                    <path d="M15.2897 10.7897C15.9591 10.1202 17.0462 10.1202 17.7157 10.7897L27.9979 21.0719C28.6674 21.7414 28.6674 22.8285 27.9979 23.4979C27.3285 24.1673 26.2414 24.1673 25.572 23.4979L16.5 14.4259L7.42804 23.4926C6.75862 24.162 5.67148 24.162 5.00206 23.4926C4.33265 22.8231 4.33265 21.736 5.00206 21.0666L15.2843 10.7843L15.2897 10.7897Z" fill="white"/>
+                </svg>
+            </div>
+            <div className="dropdown-text hidden" id={collapse.name+'text'}>
+                <p>{collapse.txt}</p>
+            </div>
+        </article>
     )
 }
 
